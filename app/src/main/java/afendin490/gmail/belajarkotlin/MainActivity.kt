008968cca91d2c56fb674b6ae451d97dbb8e246c
AfendiNahak <afendin490@gmail.com>
2019-05-08 09:25:42 +0700

    Initial commit

---
+++ app/src/main/java/afendin490/gmail/belajarkotlin/MainActivity.kt
@@ -0,0 +1,28 @@
+package afendin490.gmail.belajarkotlin
+
+import android.R.layout.simple_list_item_1
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // data yang akan kita tampilkan ke dalam ListView
+        val languages = listOf("Java", "Kotlin", "Javascript", "PHP", "Python")
+
+        // memberikan adapter ke ListView
+        lv_languages.adapter = ArrayAdapter(this, simple_list_item_1, languages)
+
+        // menangani item saat dilklik
+        lv_languages.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this, "Anda memilih: ${languages[position]}",Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
